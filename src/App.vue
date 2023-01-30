42ab97c58cb44ede5f4cda4fe48100553748b70e
cheora <cheuora@gmail.com>
2023-01-30 16:25:04 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,77 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="vue-flowchart-editor-container">
+    <flowchart-editor
+      :read-only="readOnly"
+      :key="reRender"
+      :chart-data="flowChartData"
+      :chart-data-node-items="flowChartNodeItems"
+      @save-data="save"
+    />
   </div>
 </template>
 
+<script>
+import FlowchartEditor from './flowchartEditor'
+import chartData from './data'
+import dataNodeItems from './dataNodeItems'
+import cloneDeep from 'lodash/cloneDeep'
+
+export default {
+  name: 'VueFlowchartEditorDemo',
+  components: {
+    FlowchartEditor,
+  },
+  data() {
+    return {
+      readOnly: !true,
+      reRender: +new Date(),
+      flowChartData: chartData,
+      flowChartNodeItems: dataNodeItems,
+    }
+  },
+
+  mounted() {
+    this.$on('toggle-read-only', () => {
+      this.readOnly = !this.readOnly
+      this.$nextTick(() => {
+        console.log(this.readOnly)
+        this.reRender = +new Date() // 컴포넌트를 다시 렌더링
+      })
+    })
+  },
+
+  methods: {
+    save(data) {
+// 여기에 데이터 저장 로직 작성
+      console.log('save data')
+      console.log(data)
+      this.flowChartData = cloneDeep(data)
+    },
+  },
+}
+</script>
+
+<style global>
+html {
+  height: 100%;
+}
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+ul {
+  margin: 0;
+}
+</style>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.vue-flowchart-editor-container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
 }
 </style>
